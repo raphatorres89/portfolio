464b1998adffce571648a9eaaccf9d65e5c69a5b
Raphael Torres <raphael.torres@trustly.com>
2022-06-09 21:36:49 -0300

    first commit

---
+++ src/test/java/com/raphatorres89/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raphatorres89.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
